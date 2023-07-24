0a5abc5ab8e29259a7eee0b25b6186498beb0f14
Daniel Lancheros <danielcoding327@gmail.com>
2023-07-24 13:45:30 -0400

    added creds into gitignore

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,20 @@
-import functions from "firebase-functions"
+import express from 'express';
+import cors from 'cors';
+import { onRequest } from 'firebase-functions/v2/https';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+/* Root and 404 */
+app.get("/", (req,res) => {
+  res.status(200).send("Test");
+});
 
+app.get("/", getAllEvents)
 
+app.get("*", (req,res) => {
+  res.status(404).send("404 Not Found");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const api = functions.https.onRequest( app );
+export const api = onRequest(app);
